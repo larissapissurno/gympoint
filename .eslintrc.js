3b319bf8185eb39f72df89798d08419b133fd32b
Larissa Pissurno <larissapissurno@gmail.com>
2019-10-27 16:01:06 -0400

    Autenticação de usuário

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
-    node: true,
   },
   extends: [
     'airbnb-base',
     'prettier'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'eb56ccf0ed4eb8e37bb127a7369c8fd4',
+  expiresIn: '7d',
+};
